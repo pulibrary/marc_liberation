6459d6fd8fc6f4ee1c591eeeeafe77eab6754645
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2018-04-24 02:06:52 -0400

    indexes to solr as part of voyager export process

--- app/jobs/bib_dump_job.rb
+++ app/jobs/bib_dump_job.rb
@@ -3,11 +3,22 @@ require 'voyager_helpers'
 class BibDumpJob < ActiveJob::Base
   queue_as :default
 
-  def perform(id_slice, df_id)
+  # Dump Marc XML to disk, index to solr, and zip
+  # Option to post to a separate url for reindex
+  def perform(id_slice, df_id, reindex=false)
     df = DumpFile.find(df_id)
     File.truncate(df.path, 0) if File.exist?(df.path)
     VoyagerHelpers::Liberator.dump_bibs_to_file(id_slice, df.path)
+    index(df.path, reindex)
     df.zip
     df.save
+  end
+
+  def index(path, reindex)
+    process_locations unless Rails.env.test? || !ActiveRecord::Base.connection.table_exists?('locations_holding_locations')
+    indexer = Traject::Indexer.new
+    indexer.load_config_file(Rails.application.config.traject['config'])
+    indexer.settings['solr.url'] = ENV['SOLR_REINDEX_URL'] if reindex
+    indexer.process(path)
   end
 end
--- marc_to_solr/lib/traject_config.rb
+++ marc_to_solr/lib/traject_config.rb
@@ -15,7 +15,7 @@ extend Traject::Macros::Marc21Semantics
 extend Traject::Macros::MarcFormats
 
 settings do
-  provide "solr.url", "http://localhost:8983/solr/blacklight-core-development" # default
+  provide "solr.url", ENV['SOLR_URL'] || "http://localhost:8983/solr/blacklight-core-development" # default
   provide "solr.version", "4.10.0"
   provide "marc_source.type", "xml"
   provide "solr_writer.max_skipped", "50"
