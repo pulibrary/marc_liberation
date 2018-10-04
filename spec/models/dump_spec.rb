3b061e820e215748388579d933184518ff25cb89
GriffinJ <jrgriffiniii@gmail.com>
2018-10-04 09:43:11 -0400

    Extending the test suite for holding ID and ReCAP exports

--- spec/models/dump_spec.rb
+++ spec/models/dump_spec.rb
@@ -9,17 +9,16 @@ describe Dump do
   end
 
   describe '.dump_bib_ids' do
+    subject(:dump) { described_class.dump_bib_ids }
+
     before do
       allow(VoyagerHelpers::SyncFu).to receive(:bib_ids_to_file)
-      described_class.dump_bib_ids
+      dump
     end
 
     it 'constructs an Event and exports the voyager IDs to a file' do
       event = Event.first
       expect(event).not_to be_nil
-
-      dump = Dump.find_by(event: event)
-      expect(dump).not_to be_nil
 
       dump_file = DumpFile.find_by(dump: dump)
       expect(dump_file).not_to be_nil
@@ -31,6 +30,57 @@ describe Dump do
       expect(dump_type.constant).to eq 'BIB_IDS'
       expect(VoyagerHelpers::SyncFu).to have_received(:bib_ids_to_file).with('data/1')
       expect(dump.dump_files).to eq([dump_file])
+      expect(dump.updated_at).to eq(Time.now)
+    end
+  end
+
+  describe '.dump_holding_ids' do
+    subject(:dump) { described_class.dump_holding_ids }
+
+    before do
+      allow(VoyagerHelpers::SyncFu).to receive(:holding_ids_to_file)
+      dump
+    end
+
+    it 'constructs an Event and exports the voyager IDs to a file' do
+      event = Event.first
+      expect(event).not_to be_nil
+
+      dump_file = DumpFile.find_by(dump: dump)
+      expect(dump_file).not_to be_nil
+
+      dump_type = dump.dump_type
+      expect(dump_type).not_to be_nil
+
+      expect(dump_type.label).to eq 'All Holding IDs'
+      expect(dump_type.constant).to eq 'HOLDING_IDS'
+      expect(VoyagerHelpers::SyncFu).to have_received(:holding_ids_to_file).with('data/1')
+      expect(dump.dump_files).to eq([dump_file])
+      expect(dump.updated_at).to eq(Time.now)
+    end
+  end
+
+  describe '.dump_recap_records' do
+    subject(:dump) { described_class.dump_recap_records }
+
+    let(:yesterday) { Time.now - 1.day }
+
+    before do
+      allow(VoyagerHelpers::SyncFu).to receive(:recap_barcodes_since)
+      dump
+    end
+
+    it 'constructs an Event and exports the voyager IDs to a file' do
+      event = Event.first
+      expect(event).not_to be_nil
+
+      dump_type = dump.dump_type
+      expect(dump_type).not_to be_nil
+
+      expect(dump_type.label).to eq 'Updated Princeton ReCAP Records'
+      expect(dump_type.constant).to eq 'PRINCETON_RECAP'
+      expect(VoyagerHelpers::SyncFu).to have_received(:recap_barcodes_since).with(yesterday)
+      expect(dump.updated_at).to eq(Time.now)
     end
   end
 
